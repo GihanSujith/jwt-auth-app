2ad000595ff4a653fb185d4e13970c966af4aab9
Gihan Sujith <gihansujith943@gmail.com>
2024-10-08 13:12:48 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 require('dotenv').config();
+require('./Models/db');
 
 const PORT = process.env.PORT || 8080;
 
@@ -8,6 +10,9 @@ const PORT = process.env.PORT || 8080;
 app.get('/ping', (req, res) => {
     res.send('PONG');
 })
+
+app.use(bodyParser.json());
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
