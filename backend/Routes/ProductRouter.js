e792487eadc1382b556f45c6f7a60d11cb90b853
Gihan Sujith <gihansujith943@gmail.com>
2024-10-09 08:12:40 +0530

    Home Page

--- backend/Routes/ProductRouter.js
+++ backend/Routes/ProductRouter.js
@@ -1,9 +1,10 @@
-
+const ensureAuthenticated = require('../Middlewares/Auth');
 
 const router = require('express').Router();
 
 
 router.get('/', ensureAuthenticated, (req, res) => {
+    console.log('---- logged in user detail ----', req.user);
     res.status(200).json([
         {
             name:"mobile",
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 
+import { Routes,Route, Navigate } from 'react-router-dom';
+
 import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Auth App</h1>
+      <Routes>
+        <Route path='/' element={<Navigate to="/login"/>}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/home' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
