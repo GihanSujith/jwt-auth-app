fdb994a0624630c36144f53223092c72e71b4c53
Gihan Sujith <gihansujith943@gmail.com>
2024-10-09 18:21:24 +0530

    Signup API Call

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,41 +1,101 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import {ToastContainer} from 'react-toastify';
+import { handleError, handleSuccess } from '../utils';
+
+
 
 function signup() {
+
+    const [signupInfo, setSignupInfo] = useState({
+        name:'',
+        email:'',
+        password:''
+    })
+    
+    const navigate = useNavigate();
+    const handleChange = (e)=>{
+        const {name,value} = e.target;
+        console.log(name,value);
+        const copySignupInfo = { ...signupInfo };
+        copySignupInfo[name] = value;
+        setSignupInfo(copySignupInfo);
+
+    }
+    
+    const handleSignup = async (e)=>{
+        e.preventDefault();
+        const {name,email,password}=signupInfo;
+        if(!name||!email||!password){
+            return handleError('name,email and password are required')
+        }
+        try {
+            const url = "http://localhost:8080/auth/signup";
+            const response = await fetch(url,{
+                method:"POST",
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(signupInfo)
+            });
+            const result = await response.json();
+            const {success,message,error} = result;
+            if(success){
+                handleSuccess(message);
+                setTimeout(()=>{
+                    navigate('/login')
+                },1000)
+            }else if(error){
+                const details = error?.details[0].message;
+                handleError(details);
+            }else if(!success){
+                handleError(message);
+            }
+            console.log(result);
+        } catch (err) {
+            handleError(err);
+        }
+    }
+
   return (
     <div className='container'>
         <h1>Login</h1>
-        <from>
+        <from onSubmit={handleSignup}>
             <div>
                 <label htmaFor='name'>Name</label>
                 <input
+                onChange={handleChange}
                     type='text'
                     name='name'
                     autoFocus
                     placeholder='Enter your name...'
+                    value={signupInfo.name}
                 
                 />
             </div>
             <div>
                 <label htmaFor='email'>Email</label>
                 <input
+                onChange={handleChange}
                     type='email'
                     name='email'
                     placeholder='Enter your email...'
+                    value={signupInfo.email}
                 
                 />
             </div>
             <div>
                 <label htmaFor='password'>Password</label>
                 <input
+                onChange={handleChange}
                     type='password'
                     name='password'
                     placeholder='Enter your password...'
+                    value={signupInfo.password}
                 
                 />
             </div>
-            <button>Signup</button>
+            <button type='submit'>Signup</button>
             <span>Already have an account ?
                 <Link to="/login">Login</Link>
             </span>
